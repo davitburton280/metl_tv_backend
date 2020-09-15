3def9e3acbb30a372c53f83ac6507bfc34ca9155
Daniel <danieldukas11@gmail.com>
2020-09-15 10:17:12 +0400

    Moved crypto currency to modal component in core module, installed ngx-bootstrap

--- app.js
+++ app.js
@@ -41,6 +41,33 @@ console.log(dist)
 app.use(express.static(dist));
 
 
+// Obs
+
+
+// const OBSWebSocket = require('obs-websocket-js');
+// const obs = new OBSWebSocket();
+//
+// // Declare some events to listen for.
+// obs.on('ConnectionOpened', () => {
+//     console.log('Connection Opened');
+//
+//     // Send some requests.
+//     obs.sendCallback('GetSceneList', {}, (err, data) => {
+//         console.log('Using callbacks:', err, data);
+//     });
+//
+//     obs.send('GetSceneList').then(data => {
+//         console.log('Using promises:', data);
+//     });
+// });
+//
+// obs.on('SwitchScenes', data => {
+//     console.log('SwitchScenes', data);
+// });
+//
+// obs.connect();
+
+
 // Separating Angular routes
 app.get('*', (req, res, next) => {
     if (!req.url.includes('phpmyadmin')) {
@@ -49,6 +76,7 @@ app.get('*', (req, res, next) => {
         res.status(404).send('Not found');
     }
 });
+
 
 
 
