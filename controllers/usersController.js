fafb47850642e35b62cc23e9be421fa53861791f
Daniel <danieldukas11@gmail.com>
2020-09-16 16:26:57 +0400

    Made fixes with publisher/subscriber looks

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,14 +2,38 @@
 const opentok = require('opentok');
 const OT = new opentok(process.env.OPENTOK_API_KEY, process.env.OPENTOK_API_SECRET);
 
+let sessions = {};
+
 
 exports.getOpentokParams = async (req, res) => {
-    OT.createSession((err, session) => {
-        let sessionId = session.sessionId;
-        res.json({
-            apiKey: process.env.OPENTOK_API_KEY,
-            sessionId,
-            token: OT.generateToken(sessionId, {role: 'publisher'})
+    const roomName = req.params.room;
+    console.log(roomName)
+
+    if (sessions[roomName]) {
+        generateToken(roomName, res);
+    } else {
+
+        OT.createSession((error, session) => {
+            if (error) {
+                console.log("Error creating session:", error)
+            } else {
+                sessions[roomName] = session.sessionId;
+                generateToken(roomName, res);
+            }
         })
+    }
+};
+
+generateToken = (roomName, res) => {
+    const tokenOptions = {
+        role: "publisher",
+        data: `roomname=${roomName}`
+    };
+    let token = OT.generateToken(sessions[roomName], tokenOptions);
+    res.status(200);
+    res.send({
+        sessionId: sessions[roomName],
+        token,
+        apiKey: process.env.OPENTOK_API_KEY
     })
 };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
-router.get('/get-opentok-params/', usersController.getOpentokParams);
+router.get('/session/:room', usersController.getOpentokParams);
 
 module.exports = router;
