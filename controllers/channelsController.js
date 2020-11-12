745bb450b11b332245fa19c4485e9fff70a7f817
David Burton <davitburton280@gmail.com>
2020-11-12 14:21:52 +0400

    Added videos searching mechanism

--- controllers/channelsController.js
+++ controllers/channelsController.js
@@ -3,8 +3,36 @@ const Users = db.users;
 const Channels = db.channels;
 const Videos = db.videos;
 
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
 exports.get = async (req, res) => {
     console.log('here!!!!!!!!!!!!!')
     let channels = await Channels.findAll({include: [{model: Videos}, {model: Users}]});
+    res.json(channels);
+};
+
+exports.searchChannelVideos = async (req, res) => {
+    let search = req.query.search;
+    let channels = await Channels.findAll(
+        {
+            include: [{model: Videos}, {model: Users}],
+            where: {
+                [Op.or]: [
+                    {
+                        name: {
+                            [Op.like]: '%' + search + '%'
+                        }
+                    },
+                    sequelize.where(sequelize.col('`videos`.`name`'), 'like', '%' + search + '%')
+                    // {
+                    //     id: {
+                    //         [Op.like]: '%' + search + '%'
+                    //     }
+                    // }
+                ]
+
+            }
+        });
     res.json(channels);
 };
--- routes/channels.js
+++ routes/channels.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const channelsController = require('../controllers/channelsController');
 
 router.get('/get', channelsController.get);
+router.get('/search-with-videos', channelsController.searchChannelVideos);
 
 module.exports = router;
