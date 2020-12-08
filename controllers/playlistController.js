f051fc392eb417fc072096e960ca2ce3526f2403
Davit Burton <davitburton280@gmail.com>
2020-12-08 12:23:49 +0400

    Single playlist getting test

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const Playlists = db.playlists;
+const Videos = db.videos;
 exports.add = async (req, res) => {
     const data = req.body;
     await Playlists.create(data);
@@ -8,5 +9,10 @@ exports.add = async (req, res) => {
 
 exports.get = async (req, res) => {
     const playlists = await Playlists.findAll({});
+    res.json(playlists);
+};
+
+exports.getById = async (req, res) => {
+    const playlists = await Playlists.findOne({id: req.query.id, include: [{model: Videos}]});
     res.json(playlists);
 };
--- routes/playlists.js
+++ routes/playlists.js
@@ -4,5 +4,6 @@ const playlistController = require('../controllers/playlistController');
 
 router.post('/add', playlistController.add);
 router.get('/get', playlistController.get);
+router.get('/get-by-id', playlistController.getById);
 
 module.exports = router;
