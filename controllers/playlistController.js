cc0c564ccec09654e2e7b58eb325ee4b4cf6e4a0
David Burton <davitburton280@gmail.com>
2021-02-19 11:31:42 +0400

    Added playlist backend validation, confirmation dialog when deleting a playlist, added build-play-video-route helper

--- controllers/playlistController.js
+++ controllers/playlistController.js
@@ -8,11 +8,14 @@ const sequelize = require('sequelize');
 const Op = sequelize.Op;
 const to = require('../helpers/getPromiseResult');
 const videoController = require('../controllers/videoController');
+const showIfErrors = require('../helpers/showIfErrors');
 
 exports.add = async (req, res) => {
-    const data = req.body;
-    let p = await Playlists.create(data);
-    res.json(p);
+    if (!showIfErrors(req, res)) {
+        const data = req.body;
+        let p = await Playlists.create(data);
+        res.json(p);
+    }
 };
 exports.addVideos = async (req, res) => {
     const data = req.body;
@@ -100,6 +103,17 @@ exports.getById = async (req, res) => {
     res.json(playlists);
 };
 
+exports.updatePlaylistInfo = async (req, res) => {
+    if (!showIfErrors(req, res)) {
+        let data = req.body;
+        const {id, name, description} = data
+        await Playlists.update({name: name, description: description}, {where: {id: id}});
+        req.query.playlist_id = req.body.id;
+        console.log(req.query)
+        this.getById(req, res);
+    }
+};
+
 
 exports.updatePrivacy = async (req, res) => {
     const {id, privacy} = req.body;
@@ -118,14 +132,7 @@ exports.changeThumbnail = async (req, res) => {
     this.getById(req, res);
 };
 
-exports.updatePlaylistInfo = async (req, res) => {
-    let data = req.body;
-    const {id, name, description} = data
-    await Playlists.update({name: name, description: description}, {where: {id: id}});
-    req.query.playlist_id = req.body.id;
-    console.log(req.query)
-    this.getById(req, res);
-};
+
 
 exports.updateVideoPosition = async (req, res) => {
     let {playlist_id, rows} = req.body;
--- routes/playlists.js
+++ routes/playlists.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const playlistController = require('../controllers/playlistController');
+const validatePlaylistDetails = require('../validators/validatePlaylistDetails');
 
-router.post('/add', playlistController.add);
+router.post('/add', validatePlaylistDetails.rules, playlistController.add);
 router.post('/add-videos', playlistController.addVideos);
 router.post('/add-video-to-other-playlists', playlistController.addVideosToOtherPlaylists);
 router.get('/get', playlistController.get);
 router.get('/search', playlistController.search);
 router.get('/get-by-id', playlistController.getById);
 router.put('/update-privacy', playlistController.updatePrivacy);
-router.put('/update-playlist-info', playlistController.updatePlaylistInfo);
+router.put('/update-playlist-info', validatePlaylistDetails.rules, playlistController.updatePlaylistInfo);
 router.put('/change-thumbnail', playlistController.changeThumbnail);
 router.put('/update-video-position', playlistController.updateVideoPosition);
 router.delete('/remove-video', playlistController.removeVideo);
