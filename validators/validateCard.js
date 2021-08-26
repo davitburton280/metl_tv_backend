b5f31b9e39215bdcf03757aba20efb981d7aaf5f
Clever Coder <clevercoder8@gmail.com>
2021-08-26 17:03:38 +0400

    Fixed 'No cards' bug when changing stocks sort name and added required validation to user card name

--- validators/validateCard.js
+++ validators/validateCard.js
@@ -4,6 +4,7 @@ const db = require('../models');
 const Cards = db.users_cards;
 
 const rules = [
+    body('name').isEmpty().withMessage('The card name is required'),
     body().custom(async (req) => {
         let cardsLen = await Cards.count({where: {user_id: req.user_id}});
         if (cardsLen === 3) throw new Error('We support only 3 cards per user');
