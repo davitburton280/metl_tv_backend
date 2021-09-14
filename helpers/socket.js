9df7ad04904e052299dae86b5c98fa2cc4bfe607
Clever Coder <clevercoder8@gmail.com>
2021-09-14 14:43:07 +0400

    Active user fix in show-messages chat form, private messaging

--- helpers/socket.js
+++ helpers/socket.js
@@ -1,13 +1,27 @@
+let users = [];
 exports.socket = (io) => {
     io.on('connection', (socket) => {
         console.log('new connection made');
 
+        socket.on('newUser', (username) => {
+            users[username] = socket.id;
+            console.log('USERS CONNECTED!!!')
+            console.log(users)
+            io.emit('userConnected', username)
+        })
+
         socket.on('sendMessage', (data) => {
             console.log('MESSAGE!!!')
-            console.log(data);
-            socket.broadcast.emit('newMessage', data)
+            console.log(data.to_user)
+            let username = data.to_user.from;
+            let socketId = users[username];
+            console.log(username);
+            console.log(socketId)
+            io.to(socketId).emit('newMessage', data)
+            // socket.broadcast.emit('newMessage', data)
 
         });
+
 
         socket.emit('event2', {
             msg: 'Server to client, do you read me? Over.'
