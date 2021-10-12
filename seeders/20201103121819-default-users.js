acf0c8c9c8c30f173f41bd61488104f48cc06b68
sargr86 <sargr86@gmail.com>
2021-10-12 14:06:14 +0400

    Updated default users

--- seeders/20201103121819-default-users.js
+++ seeders/20201103121819-default-users.js
@@ -21,11 +21,26 @@ module.exports = {
                 updated_at: new Date()
             },
             {
-                first_name: 'Test',
-                last_name: 'User',
+                first_name: 'Jane',
+                last_name: 'Doe',
                 birthday: '1986-03-30',
                 gender: 'female',
                 email: 'test@gmail.com',
+                password: bcrypt.hashSync(process.env.DEV_PASS, 10),
+                stocks_order_type_id: 1,
+                username: 'test',
+                avatar: '',
+                cover: '',
+                phone: '000 000 0000',
+                created_at: new Date(),
+                updated_at: new Date()
+            },
+            {
+                first_name: 'Janine',
+                last_name: 'Doe',
+                birthday: '1986-03-30',
+                gender: 'female',
+                email: 'janine@gmail.com',
                 password: bcrypt.hashSync(process.env.DEV_PASS, 10),
                 stocks_order_type_id: 1,
                 username: 'test',
