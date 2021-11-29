321925c17b9fcbd2dde88f7cec9892884da1c07c
liamw6367@gmail.com <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>
2021-11-29 18:01:22 +0400

    Connection_id fix for bottom chat, video chat requests fixes

--- controllers/chat/directChatController.js
+++ controllers/chat/directChatController.js
@@ -20,93 +20,108 @@ const m = require('../../helpers/multer');
 const moment = require('moment');
 
 exports.getDirectMessages = async (req, res) => {
-    let {user_id, blocked} = req.query;
+    let {user_id, from_id, to_id, blocked} = req.query;
 
-    let directConnectionsResult = await UsersConnectionMembers.findAll({
-        where: {member_id: user_id},
-        attributes: ['connection_id']
-    });
+    if (from_id && to_id) {
+        this.getMessagesBetweenTwoUsers(req, res);
+    } else {
 
-    let directConnectionIds = JSON.parse(JSON.stringify(directConnectionsResult)).map(t => t.connection_id);
 
-    console.log('aaaaa',directConnectionIds)
-
-    let usersMessages = await Users.findAll({
-        attributes: ['id', 'first_name', 'last_name', 'avatar', 'username'],
-        where: {
-            [Op.not]: {
-                id: user_id
-            }
-        },
-        order: [
-            [sequelize.col('`users_connections->users_messages`.`created_at`'), 'asc'],
-        ],
-        include: [
-            {
-                model: UsersConnection, as: 'users_connections',
-                attributes: ['id', 'is_blocked', 'confirmed'],
-
-                where: {
-                    id: {[Op.in]: directConnectionIds},
-                    confirmed: 1
-                },
-
-                through: {attributes: []},
-                include: [
-                    {
-                        model: DirectChatMessages,
-                        attributes: {exclude: ['group_id', 'video_id', 'connection_id', 'to_user', 'updated_at']},
-                        as: 'users_messages',
-                        include: [
-                            {
-                                model: Users,
-                                as: 'from_user',
-                                attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name']
-                            },
-                            {
-                                model: Users,
-                                as: 'to_user',
-                                attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name'],
-                            },
-                        ]
-                    }
-                ]
-            }
-        ]
-    });
-
-    let usersFiltered = [];
-    // let blockedUsers = await usersController.getBlockedContactsIds(user_id, 1);
-    let blockedUsers = [];
-
-    usersMessages.map(um => {
-        let unseens = 0;
-
-        um.users_connections[0]?.users_messages?.map(m => {
-            let msg = m.toJSON();
-            if (msg.seen === 0 && +user_id !== +m.from_id) {
-                ++unseens;
-            }
+        let directConnectionsResult = await UsersConnectionMembers.findAll({
+            where: {member_id: user_id},
+            attributes: ['connection_id']
         });
-        usersFiltered.push({unseens, ...um.toJSON()});
-    });
 
-    let ret = usersFiltered.sort((a, b) => {
-        let aMessages = a.users_connections[0]?.users_messages;
-        let bMessages = b.users_connections[0]?.users_messages;
-        return +(+moment(bMessages[bMessages.length - 1]?.created_at) - (+moment(aMessages[aMessages.length - 1]?.created_at)));
-    });
+        let directConnectionIds = JSON.parse(JSON.stringify(directConnectionsResult)).map(t => t.connection_id);
 
-    res.json(ret);
+        console.log('aaaaa', directConnectionIds)
 
+        let usersMessages = await Users.findAll({
+            attributes: ['id', 'first_name', 'last_name', 'avatar', 'username'],
+            where: {
+                [Op.not]: {
+                    id: user_id
+                }
+            },
+            order: [
+                [sequelize.col('`users_connections->users_messages`.`created_at`'), 'asc'],
+            ],
+            include: [
+                {
+                    model: UsersConnection, as: 'users_connections',
+                    attributes: ['id', 'is_blocked', 'confirmed'],
+
+                    where: {
+                        id: {[Op.in]: directConnectionIds},
+                        confirmed: 1
+                    },
+
+                    through: {attributes: []},
+                    include: [
+                        {
+                            model: DirectChatMessages,
+                            attributes: {exclude: ['group_id', 'video_id', 'connection_id', 'to_user', 'updated_at']},
+                            as: 'users_messages',
+                            include: [
+                                {
+                                    model: Users,
+                                    as: 'from_user',
+                                    attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name']
+                                },
+                                {
+                                    model: Users,
+                                    as: 'to_user',
+                                    attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name'],
+                                },
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
+
+        let usersFiltered = [];
+        // let blockedUsers = await usersController.getBlockedContactsIds(user_id, 1);
+        let blockedUsers = [];
+
+        usersMessages.map(um => {
+            let unseens = 0;
+
+            um.users_connections[0]?.users_messages?.map(m => {
+                let msg = m.toJSON();
+                if (msg.seen === 0 && +user_id !== +m.from_id) {
+                    ++unseens;
+                }
+            });
+            usersFiltered.push({unseens, ...um.toJSON()});
+        });
+
+        let ret = usersFiltered.sort((a, b) => {
+            let aMessages = a.users_connections[0]?.users_messages;
+            let bMessages = b.users_connections[0]?.users_messages;
+            return +(+moment(bMessages[bMessages.length - 1]?.created_at) - (+moment(aMessages[aMessages.length - 1]?.created_at)));
+        });
+
+        res.json(ret);
+    }
 };
 
 exports.saveDirectMessage = async (req, res) => {
     let data = req.body;
     data.message = nl2br(data.message, false);
     let msg = await to(DirectChatMessages.create(data));
+
+    if(!data.bottom_chat){
     req.query.user_id = data.from_id;
-    this.getDirectMessages(req, res);
+        this.getDirectMessages(req, res);
+    }
+    else {
+        req.query.from_id = data.from_id;
+        req.query.to_id = data.to_id;
+        this.getMessagesBetweenTwoUsers(req,res);
+    }
+
+
 };
 
 exports.updateSeen = async (data) => {
@@ -128,7 +143,7 @@ exports.unreadMessages = async (data) => {
                 id: message_ids
             }
         }
-    })
+    });
 
     let updated = await DirectChatMessages.update({seen: 0, seen_at: null}, {
         where: {
@@ -140,4 +155,38 @@ exports.unreadMessages = async (data) => {
 
     console.log(updated)
     return !!updated;
+};
+
+exports.getMessagesBetweenTwoUsers = async (req, res) => {
+    let {from_id, to_id} = req.query;
+
+    let arr = [
+        {
+            to_id: from_id,
+            from_id: to_id
+        },
+        {from_id, to_id}
+    ];
+
+
+    let m = await DirectChatMessages.findAll({
+        where: {
+            [Op.or]: arr
+        },
+        attributes: {exclude: ['updated_at']},
+        include: [
+            {
+                model: Users,
+                as: 'from_user',
+                attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name']
+            },
+            {
+                model: Users,
+                as: 'to_user',
+                attributes: [['username', 'from'], 'id', 'avatar', 'first_name', 'last_name'],
+            },
+        ]
+    });
+
+    res.json(m);
 };
--- routes/chat/direct.js
+++ routes/chat/direct.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const directChatController = require('../../controllers/chat/directChatController');
 
 router.get('/get-direct-messages', directChatController.getDirectMessages);
+router.get('/get-messages-between-two-users', directChatController.getMessagesBetweenTwoUsers);
 router.post('/save-message', directChatController.saveDirectMessage);
 
 module.exports = router;
