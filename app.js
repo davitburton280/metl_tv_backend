d2e2f03fe0d327976dac5c4621eb66f73fe12ab1
liamw6367@gmail.com <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>
2022-02-10 16:52:09 +0400

    Mongo config changes

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ const mongoose = require('mongoose');
 let mongoDB = `mongodb://${process.env.MONGO_IP_PORT}/metltv_chat`;
 
 if (process.env.NODE_ENV === 'production') {
-    mongoDB = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_IP_PORT}/metltv_chat`;
+    mongoDB = `mongodb://${encodeURIComponent(process.env.MONGO_USER)}:${encodeURIComponent(process.env.MONGO_PASS)}@${process.env.MONGO_IP_PORT}/metltv_chat`;
 }
 
 
