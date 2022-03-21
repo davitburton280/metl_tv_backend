a28ff7de4a7bce14d59a4b01c40ae228b99b2554
liamw6367@gmail.com <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>
2022-03-21 09:40:03 +0400

    Created posts model/migration

--- migrations/20220321053703-create-posts.js
+++ migrations/20220321053703-create-posts.js
@@ -1,45 +1,47 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('posts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      author_id: {
-        type: Sequelize.INTEGER
-      },
-      category_id: {
-        type: Sequelize.INTEGER
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.TEXT
-      },
-      likes: {
-        type: Sequelize.INTEGER
-      },
-      views: {
-        type: Sequelize.INTEGER
-      },
-      privacy: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('posts');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('posts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            author_id: {
+                type: Sequelize.INTEGER
+            },
+            category_id: {
+                type: Sequelize.INTEGER
+            },
+            title: {
+                type: Sequelize.STRING
+            },
+            description: {
+                type: Sequelize.TEXT
+            },
+            likes: {
+                type: Sequelize.INTEGER
+            },
+            views: {
+                type: Sequelize.INTEGER
+            },
+            privacy: {
+                type: Sequelize.INTEGER
+            },
+            created_at: {
+                defaultValue: Sequelize.fn('NOW'),
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                defaultValue: Sequelize.fn('NOW'),
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('posts');
+    }
 };
--- models/posts.js
+++ models/posts.js
@@ -1,29 +1,32 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class posts extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  posts.init({
-    author_id: DataTypes.INTEGER,
-    category_id: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    likes: DataTypes.INTEGER,
-    views: DataTypes.INTEGER,
-    privacy: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'posts',
-  });
-  return posts;
+    class posts extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    posts.init({
+        author_id: DataTypes.INTEGER,
+        category_id: DataTypes.INTEGER,
+        title: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        likes: DataTypes.INTEGER,
+        views: DataTypes.INTEGER,
+        privacy: DataTypes.INTEGER
+    }, {
+        sequelize,
+        modelName: 'posts',
+        underscored: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    });
+    return posts;
 };
