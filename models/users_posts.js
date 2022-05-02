53bdd50021e39a5deee040078a2c78fdd30be26b
davitburton280 <devmindd1@gmail.com>
2022-05-02 15:55:15 +0400

    )

--- models/users_posts.js
+++ models/users_posts.js
@@ -1,31 +1,16 @@
-'use strict';
-const {
-    Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-    class users_posts extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-        }
-    };
-    users_posts.init({
+module.exports = (sequelize, Sequelize) => {
+    const Currency = sequelize.define('posts', {
         user_id: DataTypes.INTEGER,
         post_id: DataTypes.INTEGER,
         liked: DataTypes.INTEGER,
         disliked: DataTypes.INTEGER,
         viewed: DataTypes.INTEGER,
         saved: DataTypes.INTEGER,
-        voted: DataTypes.INTEGER
-    }, {
-        sequelize,
-        modelName: 'users_posts',
-        createdAt: 'created_at',
-        updatedAt: 'updated_at'
+        voted: DataTypes.INTEGER,
+
+        createdAt: { type: Sequelize.DATE },
+        updatedAt: { type: Sequelize.DATE }
     });
-    return users_posts;
+
+    return Currency;
 };
