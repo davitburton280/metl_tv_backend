4a1ce3ad49a829a0264f9e9590cf97272844955c
devmindd1 <devmindd1@gmail.com>
2022-05-19 17:40:38 +0400

    fixings

--- controllers/channelsController.js
+++ controllers/channelsController.js
@@ -16,18 +16,18 @@ const getFullName = require('../helpers/getFullNameCol');
 
 exports.get = async (req, res) => {
     // console.log('here!!!!!!!!!!!!!')
-    let channels = await Channels.findAll({include: [{model: Videos}, {model: Users, as: 'subscribers'}]});
+    let channels = await Channels.findAll({ include: [{ model: Videos }, { model: Users, as: 'subscribers' }] });
     res.json(channels);
 };
 
 exports.getSubscriptions = async (req, res) => {
     console.log('get subscriptions!!!')
     let channels = await Users.findOne({
-        where: {id: req.query.user_id},
+        where: { id: req.query.user_id },
         include: [{
             model: Channels,
             as: 'subscriptions',
-            include: [{model: Videos, as: 'videos', attributes: ['id']}, {model: Users, as: 'user'}]
+            include: [{ model: Videos, as: 'videos', attributes: ['id'] }, { model: Users, as: 'user' }]
         }]
     });
     // console.log(channels)
@@ -35,10 +35,30 @@ exports.getSubscriptions = async (req, res) => {
 };
 
 exports.searchChannelVideos = async (req, res) => {
-    let {search, user_id, filters} = req.query;
+    let { search, user_id, filters } = req.query;
     let whereFilters = videoController.getVideoFiltersQuery(JSON.parse(filters));
+    const serachedEmementsList = search.split(' ');
     console.log('search channel with videos!!!')
     console.log(whereFilters)
+    const or = [];
+    if (serachedEmementsList.length === 1) {
+        or.push({
+            name: {
+                [Op.like]: '%' + search + '%'
+            }
+        },
+            sequelize.where(sequelize.col('`videos`.`name`'), 'like', '%' + search + '%'))
+    } else {
+        serachedEmementsList.map(item => {
+            or.push({
+                name: {
+                    [Op.like]: '%' + item + '%'
+                }
+            }) 
+        });
+        or.push(sequelize.where(sequelize.col('`videos`.`name`'), 'like', '%' + search + '%'))
+    } 
+
     // let userWhere = user_id ? sequelize.where(sequelize.col('subscribers->channel_subscribers.subscriber_id'), user_id) : {};
     if (search) {
 
@@ -46,24 +66,12 @@ exports.searchChannelVideos = async (req, res) => {
         let channels = await Channels.findAll(
             {
                 include: [
-                    {model: Videos, where: whereFilters, required: false, include: [{model: Tags, as: 'tags'}]},
-                    {model: Users, as: 'user'},
-                    {model: Users, as: 'subscribers'}
+                    { model: Videos, where: whereFilters, required: false, include: [{ model: Tags, as: 'tags' }] },
+                    { model: Users, as: 'user' },
+                    { model: Users, as: 'subscribers' }
                 ],
                 where: {
-                    [Op.or]: [
-                        {
-                            name: {
-                                [Op.like]: '%' + search + '%'
-                            }
-                        },
-                        sequelize.where(sequelize.col('`videos`.`name`'), 'like', '%' + search + '%')
-                        // {
-                        //     id: {
-                        //         [Op.like]: '%' + search + '%'
-                        //     }
-                        // }
-                    ],
+                    [Op.or]: or,
                 },
             });
         res.json(channels);
@@ -73,7 +81,7 @@ exports.searchChannelVideos = async (req, res) => {
 };
 
 exports.subscribeToChannel = async (req, res) => {
-    let {user_id, channel_id} = req.body;
+    let { user_id, channel_id } = req.body;
 
 
     let subscribedToChannel = await ChannelSubscribers.findOne({
@@ -81,20 +89,20 @@ exports.subscribeToChannel = async (req, res) => {
             subscriber_id: user_id,
             channel_id: channel_id
         },
-        attributes: {exclude: ['created_at', 'updated_at']},
+        attributes: { exclude: ['created_at', 'updated_at'] },
     });
 
     if (!subscribedToChannel) {
 
         // Getting maximum position id of the user's subscriptions
         let userSubscriptions = await ChannelSubscribers.findOne({
-            where: {subscriber_id: user_id},
+            where: { subscriber_id: user_id },
             attributes: [sequelize.fn('MAX', sequelize.col('position_id'))],
             raw: true
         });
 
         // await Channels.update();
-        let i = await Channels.increment('subscribers_count', {by: 1, where: {id: channel_id}});
+        let i = await Channels.increment('subscribers_count', { by: 1, where: { id: channel_id } });
         console.log(i)
 
         await ChannelSubscribers.create({
@@ -103,27 +111,27 @@ exports.subscribeToChannel = async (req, res) => {
             position_id: (userSubscriptions['MAX(`position_id`)'] + 1) || 1
         });
 
-        let channelSubscribers = await Channels.findOne({where: {id: channel_id}, attributes: ['subscribers_count']});
+        let channelSubscribers = await Channels.findOne({ where: { id: channel_id }, attributes: ['subscribers_count'] });
         console.log(channelSubscribers.dataValues)
-        res.json({status: 'Subscribed', ...channelSubscribers.dataValues});
+        res.json({ status: 'Subscribed', ...channelSubscribers.dataValues });
         // res.json('Subscribed');
 
     } else {
 
 
-        await Channels.decrement('subscribers_count', {by: 1, where: {id: channel_id}});
+        await Channels.decrement('subscribers_count', { by: 1, where: { id: channel_id } });
 
         await ChannelSubscribers.destroy({
             where:
-                {
-                    subscriber_id: user_id,
-                    channel_id: channel_id
-                }
+            {
+                subscriber_id: user_id,
+                channel_id: channel_id
+            }
         });
 
-        let channelSubscribers = await Channels.findOne({where: {id: channel_id}, attributes: ['subscribers_count']});
+        let channelSubscribers = await Channels.findOne({ where: { id: channel_id }, attributes: ['subscribers_count'] });
         console.log(channelSubscribers.dataValues)
-        res.json({status: 'Unsubscribed', ...channelSubscribers.dataValues});
+        res.json({ status: 'Unsubscribed', ...channelSubscribers.dataValues });
         // res.json('Unsubscribed');
     }
 
@@ -131,24 +139,24 @@ exports.subscribeToChannel = async (req, res) => {
 };
 
 exports.checkChannelSubscription = async (req, res) => {
-    let {user_id, channel_id} = req.query;
+    let { user_id, channel_id } = req.query;
 
     let channelSubscriber = await ChannelSubscribers.findOne({
         where: {
             subscriber_id: user_id,
             channel_id: channel_id
         },
-        attributes: {exclude: ['created_at', 'updated_at']},
+        attributes: { exclude: ['created_at', 'updated_at'] },
     });
 
-    let channelSubscribers = await Channels.findOne({where: {id: channel_id}, attributes: ['subscribers_count']});
+    let channelSubscribers = await Channels.findOne({ where: { id: channel_id }, attributes: ['subscribers_count'] });
     console.log(channelSubscribers.dataValues)
-    res.json({status: (channelSubscriber === null ? 'Unsubscribed' : 'Subscribed'), ...channelSubscribers.dataValues});
+    res.json({ status: (channelSubscriber === null ? 'Unsubscribed' : 'Subscribed'), ...channelSubscribers.dataValues });
 
 };
 
 exports.getSubscribers = async (req, res) => {
-    let {user_id} = req.query;
+    let { user_id } = req.query;
     let fullName = getFullName();
     console.log('user channel subscriptions!!!!!')
     let userSubscriptions = await Channels.findAll({
@@ -161,10 +169,10 @@ exports.getSubscribers = async (req, res) => {
                 where:
                     sequelize.where(sequelize.col('subscribers->channel_subscribers.subscriber_id'), user_id),
             },
-            {model: Users, attributes: ['username'], as: 'user'}
+            { model: Users, attributes: ['username'], as: 'user' }
         ],
         order: [sequelize.col('subscribers->channel_subscribers.position_id')],
-        attributes: {exclude: ['createdAt', 'updatedAt']},
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
     }).map(it => {
         it.subscribers_count = it.subscribers.length;
         return it;
@@ -174,14 +182,14 @@ exports.getSubscribers = async (req, res) => {
 };
 
 exports.changeSubscriptionPriority = async (req, res) => {
-    let {user_id, rows} = req.body;
+    let { user_id, rows } = req.body;
     rows = JSON.parse(rows);
 
     console.log(rows)
     console.log('change subscription priority!!!!!')
 
     rows.map(async (r) => {
-        await ChannelSubscribers.update({position_id: rows.indexOf(r) + 1}, {
+        await ChannelSubscribers.update({ position_id: rows.indexOf(r) + 1 }, {
             where: {
                 subscriber_id: user_id,
                 channel_id: r.id
@@ -197,14 +205,14 @@ exports.saveDescription = async (req, res) => {
     if (!showIfErrors(req, res)) {
 
 
-        const {id, ...rest} = req.body;
+        const { id, ...rest } = req.body;
         rest.description = nl2br(rest.description, false)
         req.query = req.body;
 
 
-        await Channels.update(rest, {where: {id: id}});
+        await Channels.update(rest, { where: { id: id } });
         let userInfo = await usersController.getUserInfo(req, res);
-        await usersController.changeJwt({id: id, ...rest}, res);
+        await usersController.changeJwt({ id: id, ...rest }, res);
         // res.json(userInfo);
     }
 };
@@ -212,9 +220,9 @@ exports.saveDescription = async (req, res) => {
 
 exports.saveChannelDetails = async (req, res) => {
     let data = req.body
-    const {id, ...rest} = data;
+    const { id, ...rest } = data;
     req.query = req.body;
-    await Channels.update(rest, {where: {id: id}});
+    await Channels.update(rest, { where: { id: id } });
     await usersController.changeJwt(data, res);
     // let userInfo = await usersController.getUserInfo(req, res);
     // res.json(userInfo);
@@ -222,11 +230,11 @@ exports.saveChannelDetails = async (req, res) => {
 
 
 exports.getChannelSubscribers = async (req, res) => {
-    let {id} = req.query;
+    let { id } = req.query;
     let channelSubscribers = await Channels.findOne({
-        where: {id},
+        where: { id },
         include: [
-            {model: Users, as: 'subscribers', attributes: ['id', 'avatar', 'username', 'first_name', 'last_name']}
+            { model: Users, as: 'subscribers', attributes: ['id', 'avatar', 'username', 'first_name', 'last_name'] }
         ]
     });
 
