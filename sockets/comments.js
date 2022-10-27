8b45bd25735e595384ea7dd47edcd63647130d94
prisvia <github@vault3.com>
2022-10-27 18:07:41 +0400

    new changes and branch pulling

--- sockets/comments.js
+++ sockets/comments.js
@@ -20,10 +20,10 @@ exports.store = async (data, socket, io) => {
     await socket.join(room)
     console.log(user.username, 'connected to room', room);
     // io.in(room).emit('fetchMessages', 'hello')
-    Object.keys(io.sockets.adapter.rooms).map(item => {
+    Object.keys(exsitsRoom).map(item => {
         console.log(item, 'item item imte imte');
         if (item === room) {
-            console.log(io.sockets.adapter.rooms[item], '111111111111111111111111')
+            console.log(exsitsRoom[item], '111111111111111111111111')
         }
     })
 
