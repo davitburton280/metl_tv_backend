f8bd984b2be6e7604ca236295e6080b663624c09
prisvia <github@vault3.com>
2022-10-27 17:42:10 +0400

    new changes and branch pulling

--- sockets/comments.js
+++ sockets/comments.js
@@ -16,7 +16,7 @@ exports.store = async (data, socket, io) => {
     await socket.join(`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`)
     console.log(user.username, 'connected to room', `${post_id ? 'post' : 'video'}_room_${post_id || video_id}`);
     // io.in(`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`).emit('fetchMessages', 'hello')
-    console.log(io.sockets.adapter.rooms[`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`]);
+    console.log(io.sockets.clients().length, '111111111111111111111111');
 
     return true
 
