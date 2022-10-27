f9f39f149026e3ea1a2ee11daeadc7f207d253ac
prisvia <github@vault3.com>
2022-10-27 17:22:45 +0400

    new changes and branch pulling

--- sockets/comments.js
+++ sockets/comments.js
@@ -5,6 +5,7 @@ exports.createComment = async (data, socket, io) => {
     const result = await comment_controller.create(data, user)
     if (result.success) {
         io.in(result.data.room).emit('createComment_success', JSON.stringify(result.data.comment))
+        console.log('sent createComment_success to ', result.data.room);
     }
 }
 
@@ -15,6 +16,8 @@ exports.store = async (data, socket, io) => {
     await socket.join(`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`)
     console.log(user.username, 'connected to room', `${post_id ? 'post' : 'video'}_room_${post_id || video_id}`);
     // io.in(`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`).emit('fetchMessages', 'hello')
+    io.sockets.clients(`${post_id ? 'post' : 'video'}_room_${post_id || video_id}`)
+
     return true
 
 }
