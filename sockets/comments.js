3c8a6001f08e078e9d5fd453bd448f9facd4729d
prisvia <github@vault3.com>
2022-10-28 15:36:10 +0400

    new changes and branch pulling

--- sockets/comments.js
+++ sockets/comments.js
@@ -17,7 +17,7 @@ exports.createComment = async (data, socket, io) => {
 
 exports.store = async (data, socket, io) => {
     const user = socket.decoded
-    const { post_id, video_id } = data
+    const { post_id, video_id } = JSON.parse(data)
     console.log(data, 'data ---------------------------- store');
 
     const room = `${post_id ? 'post' : 'video'}_room_${post_id || video_id}`
